9e6e0090b12e523404376797e58c0cd5020194b8
Elisa BOURG <zaza3355@hotmail.fr>
2022-02-18 09:43:59 +0100

    securise roads and prof helped us with registration

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -28,7 +28,6 @@ class RegistrationController extends AbstractController
                     $form->get('plainPassword')->getData()
                 )
             );
-
             $entityManager->persist($user);
             $entityManager->flush();
             // do anything else you need here, like send an email
