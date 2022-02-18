3c50593e09c64983de92c4b941e5dd251f90cc4f
Elisa BOURG <zaza3355@hotmail.fr>
2022-02-18 15:10:45 +0100

    add form errorsé

--- src/Form/AddMoneyType.php
+++ src/Form/AddMoneyType.php
@@ -9,13 +9,25 @@ use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Positive;
 
 class AddMoneyType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('money', NumberType::class)
+            ->add('money', NumberType::class, [
+                'label' => "Montant d'argent",
+                'constraints' => [
+                    new  Positive([
+                        'message' => 'Attention le montant doit être au minimum de 0'
+                    ]),
+                    new NotBlank([
+                        'message' => 'Merci de remplir ce champ',
+                    ]),
+                ],
+            ])
             ->add('submit', SubmitType::class)
         ;
     }
