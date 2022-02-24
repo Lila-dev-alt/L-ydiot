27de44829eb6c35f3d051df2fe915fdb4ec5fa28
Elisa BOURG <zaza3355@hotmail.fr>
2022-02-24 14:37:47 +0100

    add form

---
+++ src/Form/AskUserType.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Account;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Positive;
+
+class AskUserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            -> add('recipient', EntityType::class,[
+                'class'=>User::class ,
+                'choice_label' => 'email'] )
+            ->add('money', NumberType::class, [
+                'label' => "Montant d'argent",
+                'attr' => ['placeholder' => '200']
+            ])
+            ->add('Texte', TextType::class, [
+                'label' => "Texte a envoyer a l'autre utilisateur",
+                'attr' => ['placeholder' => '200']
+            ])
+
+            ->add('submit', SubmitType::class, [
+                'label' =>  "Envoyer",
+                'attr' => ['class' => 'btn my-[2rem] mx-auto hover:opacity-80']])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
