a277b06c11ecda849c9a3504c02a2a1c84bbf4d2
Elisa BOURG <zaza3355@hotmail.fr>
2022-03-03 15:52:55 +0100

    add admin role et add command

---
+++ src/Command/ChangeRoleUser.php
@@ -0,0 +1,50 @@
+<?php
+
+
+namespace App\Command;
+
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ChangeRoleUser extends Command
+{
+    protected static $defaultName = 'app:change-role';
+    private $repo;
+    private $em;
+
+    public function __construct(UserRepository $userRepository, EntityManagerInterface $em)
+    {
+        $this->repo = $userRepository;
+        $this->em = $em;
+        parent::__construct();
+    }
+
+
+    protected function configure(): void
+    {
+        $this
+            // ...
+            ->addArgument('id', InputArgument::REQUIRED, 'id of user')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+       $id = $input->getArgument('id');
+
+       $user = $this->repo->findOneBy(['id' => $id]);
+       $user->setRoles(['ROLE_ADMIN']);
+       $this->em->persist($user);
+       $this->em->flush();
+
+        $output->writeln('Merci !');
+
+        return Command::SUCCESS;
+    }
+
+}
